16713df53d7cf11d0a1e37fcebdd7bdeaf4a5ed9
DominicCYT <dcheung71@my.bcit.ca>
2023-11-06 12:53:23 -0800

    moving code to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-<script src="./scripts/main.js"></script>
-
 function getNameFromAuth() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
@@ -22,5 +20,47 @@ function getNameFromAuth() {
             // No user is signed in.
         }
     });
+}
+
+// Call renderPosts to render posts when the page loads
+document.addEventListener('DOMContentLoaded', renderPosts);
+function createPostCard(post) {
+  const postCard = document.createElement('div');
+  postCard.className = 'post-card';
+
+  const postImage = document.createElement('img');
+  postImage.className = 'post-image';
+  postImage.src = post.image; 
+  postImage.alt = 'Post Image';
+
+  const postContent = document.createElement('div');
+  postContent.className = 'post-content';
+
+  const postTitle = document.createElement('h2');
+  postTitle.className = 'post-title';
+  postTitle.textContent = post.title; 
+
+  const postDescription = document.createElement('p');
+  postDescription.className = 'post-description';
+  postDescription.textContent = post.description; 
+
+  const postActions = document.createElement('div');
+  postActions.className = 'post-actions';
+
+  const likeButton = document.createElement('button');
+  likeButton.className = 'like-button';
+  likeButton.textContent = `👍 ${post.Like_Num}`; 
+  attachLikeButtonListener(likeButton, post.id);
+
+  const dislikeButton = document.createElement('button');
+  dislikeButton.className = 'dislike-button';
+  dislikeButton.textContent = `👎 ${post.Dislike_Num}`; 
+
+  // Append everything to postCard
+  postActions.append(likeButton, dislikeButton);
+  postContent.append(postTitle, postDescription, postActions);
+  postCard.append(postImage, postContent);
+
+  return postCard;
 }
 getNameFromAuth(); //run the function
