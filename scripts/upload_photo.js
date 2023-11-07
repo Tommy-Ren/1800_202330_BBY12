0d9e8a39a34ff3a8330c1c12a642414e2a214a3d
Maksim Sadreev <msadreev@my.bcit.ca>
2023-11-06 20:41:36 -0800

    upload

--- scripts/upload_photo.js
+++ scripts/upload_photo.js
@@ -57,13 +57,64 @@
 //     });
 //   }
 */
-function uploadImageAndGetURL(imageFile) {
-    const storageRef = firebase.storage().ref(`images/${imageFile.name}`);
-    const uploadTask = storageRef.put(imageFile);
+
+
+
+// Now you can use ref, uploadBytes, getDownloadURL
+
+  document.getElementById('post-form').addEventListener('submit', function(event) {
+    event.preventDefault();
   
-    return uploadTask.then(() => storageRef.getDownloadURL());
-  }
+    // Get the title, description, and image file from the form
+    const title = document.getElementById('title-input').value;
+    const description = document.getElementById('description-input').value;
+    const imageFile = document.getElementById('image-input').files[0]; // This is the image file
   
+    // Check if an image was selected
+    if (imageFile) {
+      // First upload the image
+      uploadImageAndGetURL(imageFile).then((imageURL) => {
+        // Then save the post with the image URL
+        savePostToFirestore(title, description, imageURL).then(() => {
+          // Clear the form after saving
+          document.getElementById('post-form').reset();
+  
+          // Optionally re-render posts if needed
+          renderPosts();
+        });
+      }).catch((error) => {
+        console.error("Error uploading image or adding post: ", error);
+      });
+    } else {
+      // Save the post without an image URL if no image was selected
+      savePostToFirestore(title, description, null).then(() => {
+        // Clear the form after saving
+        document.getElementById('post-form').reset();
+  
+        // Optionally re-render posts if needed
+        renderPosts();
+      }).catch((error) => {
+        console.error("Error adding post: ", error);
+      });
+    }
+  });
+  function uploadImageAndGetURL(imageFile) {
+    // Assume 'firebase' is already initialized and 'storage' is firebase.storage()
+    var storageRef = firebase.storage().ref('images/' + imageFile.name);
+
+    // Upload the file to the path 'images/' in your Firebase Storage
+    return storageRef.put(imageFile).then(function(snapshot) {
+        // After the file is uploaded, get the download URL
+        return storageRef.getDownloadURL().then(function(downloadURL) {
+            console.log('File available at', downloadURL); // Log the URL
+            return downloadURL; // This URL is what you will save to Firestore
+        });
+    }).catch(function(error) {
+        // Handle any errors here
+        console.error("Error uploading file: ", error);
+        return null; // Return null if there's an error
+    });
+}
   // Function to save the post data to Firestore
   function savePostToFirestore(title, description, imageURL) {
     return db.collection('Posts').add({
@@ -74,23 +125,3 @@ function uploadImageAndGetURL(imageFile) {
       Dislike_Num: 0 
     });
   }
-  document.getElementById('post-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    // Get the title and description from the form
-    const title = document.getElementById('title-input').value;
-    const description = document.getElementById('description-input').value;
-  
-    // Save the post to Firestore without an image URL
-    savePostToFirestore(title, description, null)
-      .then(() => {
-        // Clear the form after saving
-        document.getElementById('post-form').reset();
-  
-        // Optionally re-render posts if needed
-        renderPosts();
-      })
-      .catch((error) => {
-        console.error("Error adding post: ", error);
-      });
-  });
