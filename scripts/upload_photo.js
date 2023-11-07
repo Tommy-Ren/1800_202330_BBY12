f480d52e1f51352779df85398f8a6f9f75462671
Maksim Sadreev <msadreev@my.bcit.ca>
2023-11-07 10:20:33 -0800

    upload post fix

--- scripts/upload_photo.js
+++ scripts/upload_photo.js
@@ -62,7 +62,7 @@
 
 // Now you can use ref, uploadBytes, getDownloadURL
 
-  document.getElementById('post-form').addEventListener('submit', function(event) {
+  /*document.getElementById('post-form').addEventListener('submit', function(event) {
     event.preventDefault();
   
     // Get the title, description, and image file from the form
@@ -125,3 +125,42 @@
       Dislike_Num: 0 
     });
   }
+*/
+function uploadImageAndGetURL(imageFile) {
+  const storageRef = firebase.storage().ref(`images/${imageFile.name}`);
+  const uploadTask = storageRef.put(imageFile);
+
+  return uploadTask.then(() => storageRef.getDownloadURL());
+}
+
+// Function to save the post data to Firestore
+function savePostToFirestore(title, description, imageURL) {
+  return db.collection('Posts').add({
+    title,
+    description,
+    image: imageURL,
+    Like_Num: 0, 
+    Dislike_Num: 0 
+  });
+}
+
+document.getElementById('post-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Get the title and description from the form
+  const title = document.getElementById('title-input').value;
+  const description = document.getElementById('description-input').value;
+
+  // Save the post to Firestore without an image URL
+  savePostToFirestore(title, description, null)
+    .then(() => {
+      // Clear the form after saving
+      document.getElementById('post-form').reset();
+
+      // Optionally re-render posts if needed
+      renderPosts();
+    })
+    .catch((error) => {
+      console.error("Error adding post: ", error);
+    });
+});
