3822cc70203e2c4f565de99fd5baeb5bbcc3d684
DominicCYT <dcheung71@my.bcit.ca>
2023-11-12 15:04:52 -0800

    added basic search functionality to search_page.html

---
+++ scripts/search_page.js
@@ -0,0 +1,23 @@
+
+function search() {
+    const searchQuery = document.getElementById("searchInput").value;
+
+    // Query Firestore based on the search input
+    db.collection("posts")
+      .where("tag", "==", searchQuery)
+      .get()
+      .then((querySnapshot) => {
+        const resultsContainer = document.getElementById("searchResults");
+        resultsContainer.innerHTML = ""; // Clear previous results
+
+        querySnapshot.forEach((doc) => {
+          // Display each result in the results container
+          const resultDiv = document.createElement("div");
+          resultDiv.textContent = doc.data().title; // Replace fieldName with the actual field name in your document
+          resultsContainer.appendChild(resultDiv);
+        });
+      })
+      .catch((error) => {
+        console.error("Error searching Firestore: ", error);
+      });
+  }
