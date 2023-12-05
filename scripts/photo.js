f915617fd3cf77f7aa79912f86b646b6882dc4b1
Maksim Sadreev <msadreev@my.bcit.ca>
2023-12-04 20:29:13 -0800

    camera fix test

--- scripts/photo.js
+++ scripts/photo.js
@@ -14,31 +14,40 @@ navigator.mediaDevices.getUserMedia({ video: true })
 
 // Function to capture image from video stream
 function captureImage() {
-  const video = document.getElementById('video');
-  video.play();
-  const canvas = document.createElement('canvas');
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.getContext('2d').drawImage(video, 0, 0);
+  return new Promise((resolve, reject) => {
+    const video = document.getElementById('video');
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
 
-  // Draw the video frame to the canvas
-  canvas.getContext('2d').drawImage(video, 0, 0);
+    // Draw the video frame to the canvas
+    canvas.getContext('2d').drawImage(video, 0, 0);
 
-  // Stop video playback
-  video.pause();
-  videoStream.getTracks().forEach(track => track.stop());
-
-  return canvas;
+    // Convert canvas to blob and resolve the promise
+    canvas.toBlob(blob => {
+      if (blob && blob.size > 0) {
+        resolve(blob);
+      } else {
+        reject('Blob is empty or not created.');
+      }
+    }, 'image/jpeg');
+  });
 }
 
 snap.addEventListener('click', () => {
-  const canvas = captureImage();
+  captureImage().then(blob => {
+    const reader = new FileReader();
+    reader.onloadend = function() {
+      const base64data = reader.result;
+      localStorage.setItem('capturedImage', base64data);
+      // Redirect to the upload page
+      window.location.href = './upload_photo.html';
+    };
+    reader.readAsDataURL(blob);
 
-  // Convert canvas to blob
-  canvas.toBlob(function (blob) {
-    // Save the Blob in localStorage
-    localStorage.setItem('capturedImage', blob);
-    // Redirect to the upload page
-    window.location.href = './upload_photo.html';
-  }, 'image/jpeg');
+    // Stop the video stream
+    videoStream.getTracks().forEach(track => track.stop());
+  }).catch(error => {
+    console.error(error);
+  });
 });
